4c75e15fc97091afff7a12307eb10f9b5eb02e94
Juergen May <juergen.may@latcham.co.uk>
2022-01-12 15:07:53 +0000

    customquantity

--- app/js/directives/customquantity.js
+++ app/js/directives/customquantity.js
@@ -1,18 +1,7 @@
 four51.app.directive('customquantity', function() {
   var obj = {
     restrict: 'E',
-    templateUrl: 'partials/controls/customQuantity.html',
-	   controller: ['$scope', function($scope) {
-       var quantityMultiplier = lineitem.PriceSchedule.QuantityMultiplier;
-       var unitOfMeasure = lineitem.Product.UnitOfMeasure;
-
-       if ( unitOfMeasure === '' ) {
-         $scope.quantity_text = '';
-       } else {
-         $scope.quantity_text = 'Supplied in ' + unitOfMeasure + ' of ' + quantityMultiplier;
-       }
-
-     }]
-   }
-   return obj;
- });
+    templateUrl: 'partials/controls/customQuantity.html'
+  };
+  return obj;
+});
