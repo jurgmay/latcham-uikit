1dcf71ec7d7d4cfc1430cdf31a5fba5e0482c3f5
Juergen May <juergen.may@latcham.co.uk>
2022-01-13 09:40:35 +0000

    shortProductView

--- app/js/controllers/shortProductViewCtrl.js
+++ app/js/controllers/shortProductViewCtrl.js
@@ -3,4 +3,12 @@ four51.app.controller('shortProductViewCtrl', ['$routeParams', '$scope', 'Produc
 	$scope.LineItem.Product = $scope.p;
 	ProductDisplayService.setNewLineItemScope($scope);
 	ProductDisplayService.setProductViewScope($scope);
+
+	// Split product code with a carriage return if it contains ' - '
+	var productCode = $scope.LineItem.Product.Name; // "NBT-PACK-400-BRISTOL - 30/04/2023 202AAD-L21";
+	var separators = [' - '];
+	var result = productCode.split(new RegExp(separators.join('|'), 'g'));
+	// console.log(result.join(String.fromCharCode(10)));
+	$scope.LineItem.Product.ProductCodeText = join(String.fromCharCode(10));
+
 }]);
